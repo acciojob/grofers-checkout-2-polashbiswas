1349e4d013388e75c601f9d2b168ac0f6e453f21
polashbiswas <30291056+polashbiswas@users.noreply.github.com>
2023-04-28 16:51:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 //your code here
-function calculateTotalPrice(){
-	const priceElements = document.querySelectorAll('[data-ns-test="prices"]');
+function calculateTotalPrice() {
+      const priceElements = document.querySelectorAll('[data-ns-test="prices"]');
       let totalPrice = 0;
 
       priceElements.forEach((element) => {
         const price = parseFloat(element.textContent);
         totalPrice += price;
       });
-	const grandTotalRow = document.createElement('tr');
+
+      const grandTotalRow = document.createElement('tr');
       const grandTotalCell = document.createElement('td');
       grandTotalCell.setAttribute('colspan', '2');
       grandTotalCell.setAttribute('data-ns-test', 'grandTotal');
@@ -16,5 +17,6 @@ function calculateTotalPrice(){
 
       const groceryTable = document.getElementById('groceryTable');
       groceryTable.appendChild(grandTotalRow);
-}
-calculateTotalPrice();
+    }
+
+    calculateTotalPrice();
